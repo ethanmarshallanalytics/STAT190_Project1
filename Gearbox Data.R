ec5d9c587a9ae7a8d80fca00a898eb5984cbfdc8
ethanmarshallanalytics <ethan.marshall@drake.edu>
2023-02-08 09:59:05 -0600

    Added Gearbox 3+4

--- Gearbox Data.R
+++ Gearbox Data.R
@@ -47,20 +47,39 @@ write.csv(combined_data_2, "Gearbox HS Bearing Temp Part 2/combined_data_2.csv")
 
 
 ## ------------------------------------------Gearbox Bearing Part 3
-sensor_files_3 <- list.files("Gearbox HS Bearing Temp Part 3/Drake Analytics Data Sets 2 Part 2")
+sensor_files_3 <- list.files("Gearbox HS Bearing Temp Part 3/Gearbox HS Bearing Temp Part 3")
 sensor_files_3
 
 combined_data_3 <- data.frame()
 
 # Loop through the list of files
-for (file in sensor_files[-1]) {
+for (file in sensor_files_3[-1]) {
   
-  
-  data <- read.csv(paste0("Gearbox HS Bearing Temp Part 1/Drake Analytics Data Sets 2 Part 1/", file), header = FALSE)
+  data <- read.csv(paste0("Gearbox HS Bearing Temp Part 3/Gearbox HS Bearing Temp Part 3/", file), header = FALSE)
   
   # Append the data to the combined_data data frame
-  combined_data_3 <- rbind(combined_data_23, data)
+  combined_data_3 <- rbind(combined_data_3, data)
 }
 
+
 # Write the combined data to a file
-write.csv(combined_data_2, "Gearbox HS Bearing Temp Part 1/combined_data_1.csv")
+write.csv(combined_data_3, "Gearbox HS Bearing Temp Part 3/combined_data_3.csv")
+
+## ------------------------------------------Gearbox Oil Temp
+sensor_files_4 <- list.files("Gearbox Oil Temperature/Gearbox Oil Temperature")
+sensor_files_4
+
+combined_data_4 <- data.frame()
+
+# Loop through the list of files
+for (file in sensor_files_4[-1]) {
+  
+  data <- read.csv(paste0("Gearbox Oil Temperature/Gearbox Oil Temperature/", file), header = FALSE)
+  
+  # Append the data to the combined_data data frame
+  combined_data_4 <- rbind(combined_data_4, data)
+}
+
+
+# Write the combined data to a file
+write.csv(combined_data_4, "Gearbox Oil Temperature/combined_data_4.csv")
