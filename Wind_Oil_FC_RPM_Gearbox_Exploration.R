680cf20d2ae7d37a1d689eb65be4de8f7cbd6d55
Nick PIttman <nicholasjohn.pittman@gmail.com>
2023-02-15 17:26:07 -0600

    Data prep updates
    Merge datasets on new aggregated timestamp
    Include Active Power data in merge

--- Wind_Oil_FC_RPM_Gearbox_Exploration.R
+++ Wind_Oil_FC_RPM_Gearbox_Exploration.R
@@ -4,6 +4,7 @@ library(tidyverse)
 library(forcats)
 library(lubridate)
 
+## DATA PREP -------
 # read in windspeed.csv
 wind <- read.csv("Project1Data/windspeed.csv")
 # read in gearbox_oil_temp.csv
@@ -17,8 +18,30 @@ wo <- read.csv("Project1Data/work order scrubbed.csv")
 # read in gearbox 1 and 2 data
 g1 <- read.csv("Project1Data/gearbox_1.csv")
 g2 <- read.csv("Project1Data/gearbox_2.csv")
+g3 <- read.csv("Project1Data/gearbox_3.csv")
 # read in active power data
 ap <- read.csv("Project1Data/active_power.csv")
+
+# convert datetimes and aggregate fault code times
+fc$V2 <- ymd_hms(fc$V2) # change from chr to timestamp
+fc$V3 <- ymd(fc$V3) # change from chr to date
+fc$Round_Time <- round_date(fc$V2, "10 minute")
+
+# function to change data types, group on 10 minute intervals, and find average value
+grouping <- function(data){
+  data$V2 <- ymd_hms(data$V2)
+  data$Round_Time <- round_date(data$V2, "10 minute")
+  data <- data %>% group_by(V1, Round_Time) %>% summarise(Avg_Value = mean(V4, na.rm=TRUE))
+}
+
+# call functions on each dataset
+wind = grouping(wind)
+oil_temp = grouping(oil_temp)
+rpm = grouping(rpm)
+g1 = grouping(g1)
+g2 = grouping(g2)
+g3 = grouping(g3)
+ap = grouping(ap)
 
 # filter data to only include Turbine 7
 wind_7 <- wind %>% filter(V1 == "Turbine 7")
@@ -31,30 +54,45 @@ fc_7 <- fc %>% filter(V1 == "Turbine 7")
 wo_7 <- wo %>% filter(location_id == "Turbine 7" & component_type != "null")
 
 
-# convert datetime in fault codes and aggregate fault code times
-fc_7$V2 <- ymd_hms(fc_7$V2)
-fc_7$Round_Time <- round_date(fc_7$V2, "10 minute")
-
-# function to change data types, group on 10 minute intervals, and find average value
-# TODO: group on turbine when scaled up
-grouping <- function(data){
-  data$V2 <- ymd_hms(data$V2)
-  data$Round_Time <- round_date(data$V2, "10 minute")
-  data <- data %>% group_by(Round_Time) %>% summarise(Avg_Value = mean(V4, na.rm=TRUE))
-}
-
-# call functions on each dataset
-wind_7 = grouping(wind_7)
-oil_temp_7 = grouping(oil_temp_7)
-rpm_7 = grouping(rpm_7)
-g1_7 = grouping(g1_7)
-g2_7 = grouping(g2_7)
-ap_7 = grouping(ap_7)
-
-
 # combine gearbox data together
 gearbox_7 <- rbind(g1_7, g2_7)
 
+# join fault code data to other data sets
+df1 <- fc_7 %>% 
+    left_join(oil_temp_7, by=c("Round_Time"="Round_Time")) %>% 
+    left_join(rpm_7, by=c("Round_Time"="Round_Time")) %>%
+    left_join(wind_7, by=c("Round_Time"="Round_Time")) %>%
+    left_join(gearbox_7, by=c("Round_Time"="Round_Time")) %>%
+    left_join(ap_7, by=c("Round_Time"="Round_Time"))
+
+# remove unnecessary columns
+df1 = subset(df1, select = -c(X, V1.y, V1.x.x, V1.y.y, V1.x.x.x, V1.y.y.y))
+
+# rename remaining columns to something meaningful
+df1 <- df1 %>%
+    rename("Turbine" = "V1.x",
+           "Datetime" = "V2",
+           "Date" = "V3",
+           "Fault_Code" = "V4",
+           "Status" = "V5",
+           "Fault_Description" = "V6",
+           "Fault_Type" = "V7",
+           "Oil_Temp" = "Avg_Value.x",
+           "Generator_RPM" = "Avg_Value.y",
+           "Wind_Speed" = "Avg_Value.x.x",
+           "Gearbox_Temp" = "Avg_Value.y.y",
+           "Active_Power" = "Avg_Value")
+
+# TODO: INTERPOLATE MEDIAN/MEAN FOR NULL VALUES
+
+# only return distinct columns
+df1 <- distinct(df1)
+
+# look at data tables
+View(df1)
+View(wo_7)
+
+## EXPLORATORY ANALYSIS -------
 # histogram of windspeed data (smallest dataset)
 ggplot(data = wind_7) + 
   geom_histogram(aes(x=V4), bins = 60) + xlab("Windspeed") + ylab("Frequency")
@@ -70,46 +108,6 @@ ggplot(data = rpm_7) +
 # histogram of gearbox data
 ggplot(data = gearbox_7) +
   geom_histogram(aes(x=V4), bins= 60) + xlab("Gearbox Temp") + ylab("Frequency")
-
-# join fault code data to other data sets
-df1 <- fc_7 %>% 
-    left_join(oil_temp_7, by=c("V2"="V2")) %>% 
-    left_join(rpm_7, by=c("V2"="V2")) %>%
-    left_join(wind_7, by=c("V2"="V2")) %>%
-    left_join(gearbox_7, by=c("V2"="V2"))
-
-# remove unnecessary columns
-df1 = subset(df1, select = -c(X.x, X.y, V1.y, V3.y, X.x.x, V1.x.x, V3.x.x, 
-                              X.y.y, V1.y.y, V3.y.y, X, V1, V3))
-
-# rename remaining columns to something meaningful
-df1 <- df1 %>%
-    rename("Turbine" = "V1.x",
-           "Datetime" = "V2",
-           "Date" = "V3.x",
-           "Fault_Code" = "V4.x",
-           "Status" = "V5.x",
-           "Fault_Description" = "V6",
-           "Fault_Type" = "V7",
-           "Oil_Temp" = "V4.y", 
-           "Oil_Type" = "V5.y",
-           "Generator_RPM" = "V4.x.x", 
-           "RPM_Type" = "V5.x.x",
-           "Wind_Speed" = "V4.y.y",
-           "Wind_Type" = "V5.y.y",
-           "Gearbox_Temp" = "V4",
-           "Gearbox_Type" = "V5")
-
-# only return distinct columns
-df1 <- distinct(df1)
-
-# look at data tables
-View(df1)
-View(wo_7)
-
-# filter out null rows for oil_temp, rpm, and gearbox
-## NOTE: MAY NOT WANT TO DO THIS TO KEEP ALL RAW DATA
-# df1 <- df1 %>% filter(!is.na(Oil_Temp) & !is.na(Generator_RPM) & !is.na(Gearbox_Number))
 
 #----- Analyzing the lead up to 06/05/2020 Work Order: Converter Controller - Reapired
 head(wo_7,1)
