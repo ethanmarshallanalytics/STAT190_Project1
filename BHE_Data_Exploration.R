149c178bda76a1d64d886a081359658637ab2586
Nick PIttman <nicholasjohn.pittman@gmail.com>
2023-02-22 15:46:50 -0600

    slight adjustments to improve visualizations

--- BHE_Data_Exploration.R
+++ BHE_Data_Exploration.R
@@ -32,14 +32,15 @@ Vars = data.frame(data_7$Oil_Temp, data_7$Generator_RPM, data_7$Wind_Speed, data
 cor(cbind(Vars), use="pairwise.complete.obs")
 
 ## HIGHEST CORRELATIONS FOR TURBINE 7
-## 1) Active Power & Generator RPM 0.656
-## 2) Active Power & Gearbox Temp 0.516
-## 3) Generator RPM & Oil Temp 0.449
-## 4) Active Power & Oil Temp 0.412
-
+## 1) Active Power & Generator RPM --> 0.656
+## 2) Active Power & Gearbox Temp --> 0.516
+## 3) Generator RPM & Oil Temp --> 0.449
+## 4) Active Power & Oil Temp --> 0.412
+## 5) Gearbox Temp & Oil Temp --> 0.409
+## 6) Gearbox Temp & Generator RPM --> 0.394
 
 #### TURBINE 7
-## 1) Active Power & Generator RPM 0.656
+## 1) Active Power & Generator RPM
 ggplot(data=data_7) +
   geom_point(aes(x=Generator_RPM, y = Active_Power, color = Is_Fault)) +
   geom_jitter(aes(x=Generator_RPM, y = Active_Power, color = Is_Fault), alpha=I(0.5)) +
@@ -48,14 +49,15 @@ ggplot(data=data_7) +
   theme_bw()
 
 
-## (2) Multivariate plot looking at Gearbox Temp and Active Power
+## 2) Gearbox Temp and Active Power
 ggplot(data = data_7) +
   geom_point(aes(x=Gearbox_Temp, y=Active_Power, color = Is_Fault)) +
   geom_jitter(aes(x=Gearbox_Temp, y=Active_Power, color = Is_Fault), alpha=I(0.5)) +
   labs(x = "Gearbox Temperature (ºC)", y = "Active Power (kw)", color = "Fault Status") +
-  ggtitle("Active Power vs. Gearbox Temperature")
+  ggtitle("Active Power vs. Gearbox Temperature") +
+  theme_bw()
 
-## 3) Generator RPM & Oil Temp 0.449
+## 3) Generator RPM & Oil Temp 
 ggplot(data=data_7) +
   geom_point(aes(x=Generator_RPM, y = Oil_Temp, color = Is_Fault)) +
   geom_jitter(aes(x=Generator_RPM, y = Oil_Temp, color = Is_Fault), alpha=I(0.5)) +
@@ -63,10 +65,18 @@ ggplot(data=data_7) +
   ggtitle("Oil Temperature vs. Generator RPM") +
   theme_bw()
 
-## 4) Active Power & Oil Temp 0.412
+## 4) Active Power & Oil Temp
 ggplot(data=data_7) +
   geom_point(aes(x=Active_Power, y = Oil_Temp, color = Is_Fault)) +
   geom_jitter(aes(x=Active_Power, y = Oil_Temp, color = Is_Fault), alpha=I(0.5)) +
   labs(x = "Active Power (kW)", y = "Oil Temperature (ºC)", color = "Fault Status") +
   ggtitle("Oil Temperature vs. Active Power") +
+  theme_bw()
+
+## 5) Gearbox Temp & Oil Temp
+ggplot(data=data_7) +
+  geom_point(aes(x=Gearbox_Temp, y = Oil_Temp, color = Is_Fault)) +
+  geom_jitter(aes(x=Gearbox_Temp, y = Oil_Temp, color = Is_Fault), alpha=I(0.5)) +
+  labs(x = "Gearbox Temperature (ºC)", y = "Oil Temperature (ºC)", color = "Fault Status") +
+  ggtitle("Oil Temperature vs. Gearbox Temperature") +
   theme_bw()
