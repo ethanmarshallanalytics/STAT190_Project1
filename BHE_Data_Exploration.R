930371214d88b4d7b1baffaca2707f5abbd19265
Nick PIttman <nicholasjohn.pittman@gmail.com>
2023-02-21 20:35:10 -0600

    many more plots

--- BHE_Data_Exploration.R
+++ BHE_Data_Exploration.R
@@ -8,31 +8,42 @@ library(ggplot2)
 ## DATA PREP -------
 # Read in Clean Data
 clean_data = read.csv("Project1Data/clean_BHE_data.csv")
+# slight correction in Is_Fault column to deal with missing values
+clean_data$Is_Fault[is.na(clean_data$Is_Fault)] = 0
+
+# Read in work order data
+wo = read.csv("Project1Data/work order scrubbed.csv")
 
 # filter data to only include Turbine 7
 data_7 <- clean_data %>% filter(Turbine == "Turbine 7")
+data_7_faults <- clean_data %>% filter(Turbine == "Turbine 7" & Is_Fault == 1)
+data_7_no_faults <- clean_data %>% filter(Turbine == "Turbine 7" & Is_Fault == 0)
+
+# filter data to only include Turbine 10
+data_12 <- clean_data %>% filter(Turbine == "Turbine 12")
 
 # look at data tables
 View(clean_data)
+View(wo)
 
 ## EXPLORATORY ANALYSIS -------
 # histogram of windspeed data (smallest dataset)
-ggplot(data = wind_7) + 
-  geom_histogram(aes(x=V4), bins = 60) + xlab("Windspeed") + ylab("Frequency")
+ggplot(data = data_7) + 
+  geom_histogram(aes(x=Wind_Speed), bins = 60) + xlab("Windspeed") + ylab("Frequency")
 
 # histogram of oil temperatures
-ggplot(data = oil_temp_7) +
-  geom_bar(aes(x=V4)) + xlab("Oil Temperature") + ylab("Frequency")
+ggplot(data = data_7) +
+  geom_histogram(aes(x=Oil_Temp), bins = 60) + xlab("Oil Temperature") + ylab("Frequency")
 
 # histogram of generator RPM
-ggplot(data = rpm_7) +
-  geom_histogram(aes(x=V4), bins= 100) + xlab("Generator RPM") + ylab("Frequency")
+ggplot(data = data_7) +
+  geom_histogram(aes(x=Generator_RPM), bins= 100) + xlab("Generator RPM") + ylab("Frequency")
 
 # histogram of gearbox data
-ggplot(data = gearbox_7) +
-  geom_histogram(aes(x=V4), bins= 60) + xlab("Gearbox Temp") + ylab("Frequency")
+ggplot(data = data_7) +
+  geom_histogram(aes(x=Gearbox_Temp), bins= 60) + xlab("Gearbox Temp") + ylab("Frequency")
 
-#----- Analyzing the lead up to 06/05/2020 Work Order: Converter Controller - Reapired
+#----- Analyzing the lead up to 06/05/2020 Work Order: Converter Controller - Repaired
 head(wo_7,1)
 df1$dates = as.Date(df1$Date) #Change from Character to Date
 # New Data frame looking at Fault Codes from 05/22/2020 - 06/05/2020
@@ -45,23 +56,77 @@ ggplot(data = df2) +
   coord_flip()
 
 ## MULTIVARIATE PLOTS ------------
-ggplot(data = data_7) +
-  geom_point(aes(x=Wind_Speed, y=Generator_RPM)) +
-  geom_text(aes(x=Wind_Speed, y=Generator_RPM, label=ifelse(Wind_Speed>26, paste0(Fault_Description),"")))
-
-
+#### TURBINE 7
 ## Multivariate plot looking at Wind Speed and Generator RPM
 ggplot(data = data_7) +
   geom_point(aes(x=Wind_Speed, y = Generator_RPM, color = Is_Fault)) +
- # geom_text(aes(x=Wind_Speed, y=Generator_RPM, label=ifelse(Wind_Speed>26, paste0(Fault_Description),""))) +
-  labs(x = "Wind Speed", y = "Generator RPM", color = "Fault Status")
+  # geom_text(aes(x=Wind_Speed, y=Generator_RPM, label=ifelse(Wind_Speed>26, paste0(Fault_Code),""))) +
+  geom_jitter(aes(x=Wind_Speed, y = Generator_RPM, color=Is_Fault), alpha = I(0.7)) +
+  labs(x = "Wind Speed", y = "Generator RPM", color = "Fault Status") +
+  scale_y_continuous(limits=c(0,1500)) + scale_x_continuous(limits=c(20,28))
+
+ggplot(data = data_7_faults) +
+  geom_point(aes(x=Wind_Speed, y = Generator_RPM)) +
+  # geom_text(aes(x=Wind_Speed, y=Generator_RPM, label=ifelse(Wind_Speed>26, paste0(Fault_Code),""))) +
+  geom_jitter(aes(x=Wind_Speed, y = Generator_RPM), alpha = I(0.7)) +
+  labs(x = "Wind Speed", y = "Generator RPM") +
+  scale_y_continuous(limits=c(0,1500)) + scale_x_continuous(limits=c(20,28))
+
+ggplot(data = data_7_no_faults) +
+  geom_point(aes(x=Wind_Speed, y = Generator_RPM)) +
+  # geom_text(aes(x=Wind_Speed, y=Generator_RPM, label=ifelse(Wind_Speed>26, paste0(Fault_Code),""))) +
+  geom_jitter(aes(x=Wind_Speed, y = Generator_RPM), alpha = I(0.7)) +
+  labs(x = "Wind Speed", y = "Generator RPM") +
+  scale_y_continuous(limits=c(0,1500)) + scale_x_continuous(limits=c(20,28))
 
 ## Multivariate plot looking at Wind Speed and Oil Temp
 ggplot(data = data_7) +
   geom_point(aes(x=Wind_Speed, y = Oil_Temp, color = Is_Fault)) +
-  labs(x = "Wind Speed", y = "Oil Temp", color = "Fault Status")
+  labs(x = "Wind Speed", y = "Oil Temp", color = "Fault Status") +
+  scale_y_continuous(limits=c(20,75)) + scale_x_continuous(limits=c(20,30))
+
+ggplot(data = data_7_faults) +
+  geom_point(aes(x=Wind_Speed, y = Oil_Temp)) +
+  labs(x = "Wind Speed", y = "Oil Temp") +
+  scale_y_continuous(limits=c(20,75)) + scale_x_continuous(limits=c(20,30))
+
+ggplot(data = data_7_no_faults) +
+  geom_point(aes(x=Wind_Speed, y = Oil_Temp)) +
+  labs(x = "Wind Speed", y = "Oil Temp") +
+  scale_y_continuous(limits=c(20,75)) + scale_x_continuous(limits=c(20,30))
 
 ## Multivariate plot looking at Gearbox Temp and Active Power
 ggplot(data = data_7) +
   geom_point(aes(x=Gearbox_Temp, y = Active_Power, color = Is_Fault)) +
-  labs(x = "Gearbox Temp", y = "Active Power", color = "Fault Status")
+  labs(x = "Gearbox Temp", y = "Active Power", color = "Fault Status") +
+  scale_y_continuous(limits=c(0, 3000)) + scale_x_continuous(limits=c(20, 100))
+
+ggplot(data = data_7_faults) +
+  geom_point(aes(x=Gearbox_Temp, y = Active_Power)) +
+  labs(x = "Gearbox Temp", y = "Active Power") +
+  scale_y_continuous(limits=c(0, 3000)) + scale_x_continuous(limits=c(20, 100))
+
+ggplot(data = data_7_no_faults) +
+  geom_point(aes(x=Gearbox_Temp, y = Active_Power)) +
+  labs(x = "Gearbox Temp", y = "Active Power") +
+  scale_y_continuous(limits=c(0, 3000)) + scale_x_continuous(limits=c(20, 100))
+
+## Multivariate plot looking at Wind Speed and Active Power
+ggplot(data = data_7) +
+  geom_point(aes(x=Wind_Speed, y = Active_Power, color = Is_Fault)) +
+  labs(x = "Wind Speed", y = "Active Power", color = "Fault Status") +
+  scale_y_continuous(limits=c(0,3000)) + scale_x_continuous(limits=c(20,30))
+
+ggplot(data = data_7_faults) +
+  geom_point(aes(x=Wind_Speed, y = Active_Power)) +
+  labs(x = "Wind Speed", y = "Active Power") +
+  scale_y_continuous(limits=c(0,3000)) + scale_x_continuous(limits=c(20,30))
+
+ggplot(data = data_7_no_faults) +
+  geom_point(aes(x=Wind_Speed, y = Active_Power)) +
+  labs(x = "Wind Speed", y = "Active Power") +
+  scale_y_continuous(limits=c(0,3000)) + scale_x_continuous(limits=c(20,30))
+
+
+
+###### TURBINE 12
