aa858b6c2f862e1b9051ab6a6193401b3bbdc8a5
Nick PIttman <nicholasjohn.pittman@gmail.com>
2023-03-04 20:30:07 -0600

    Scatter Plot matrix and new bar chart of fault descriptions
    NOTE: update when Daniel comes back with info on true fault data

--- BHE_Data_Exploration.R
+++ BHE_Data_Exploration.R
@@ -4,6 +4,11 @@ library(tidyverse)
 library(forcats)
 library(lubridate)
 library(ggplot2)
+# install.packages("GGally")
+library(GGally)
+# install.packages("plotly")
+library(plotly)
+library(scales)
 
 ## DATA PREP -------
 # Read in Clean Data
@@ -25,10 +30,30 @@ data_12 <- clean_data %>% filter(Turbine == "Turbine 12")
 View(clean_data)
 View(wo)
 
-## MULTIVARIATE PLOTS ------------
+## UNI- AND MULTI-VARIATE PLOTS ------------
+# further subset Turbine 7 data to only include data since Jan 1, 2022
+data_7_sub <- filter(data_7, Date > "2022-01-01")
+
+# subset to sensor data columns
+Vars = data.frame(data_7_sub$Oil_Temp, data_7_sub$Generator_RPM, data_7_sub$Wind_Speed, 
+                  data_7_sub$Gearbox_Temp, data_7_sub$Active_Power, data_7_sub$Ambient_Temp)
+
+# rename columns to something cleaner
+Vars = Vars %>% 
+  rename("Oil_Temp" = "data_7_sub.Oil_Temp",
+         "Generator_RPM" = "data_7_sub.Generator_RPM", 
+         "Wind_Speed" = "data_7_sub.Wind_Speed",
+         "Gearbox_Temp" = "data_7_sub.Gearbox_Temp",
+         "Active_Power" = "data_7_sub.Active_Power",
+         "Ambient_Temp" = "data_7_sub.Ambient_Temp")
+
 ## Correlation Matrix
-Vars = data.frame(data_7$Oil_Temp, data_7$Generator_RPM, data_7$Wind_Speed, data_7$Gearbox_Temp, data_7$Active_Power)
 cor(cbind(Vars), use="pairwise.complete.obs")
+
+## Scatter plot matrix
+p <- ggpairs(Vars, title="Sensor Data Scatter Plot Matrix") 
+ggplotly(p)
+
 
 ## HIGHEST CORRELATIONS FOR TURBINE 7
 ## 1) Active Power & Generator RPM --> 0.656
@@ -88,12 +113,31 @@ ggplot(data=data_7) +
   ggtitle("Wind Speed vs Active Power") +
   theme_bw()
 
+## bar chart of fault codes for turbine 7
+# Count of total fault codes grouped by description
+agg_fault <- data_7_faults %>%
+    group_by(Fault_Code, Fault_Description) %>%
+    summarise(total_count = n(), .groups = 'drop') %>%
+    as.data.frame()
+
+# Only select the top 5 fault descriptions
+agg_fault %>%
+  arrange(desc(total_count)) %>%
+    slice(1:5) %>%
+      ggplot(., aes(x=Fault_Description, y=total_count)) +
+      geom_bar(stat='identity') + 
+      labs(x="Fault Description", y="Frequency") +
+      ggtitle("Top 5 Most Frequent Fault Descriptions for Turbine 7") +
+      scale_y_continuous(label=comma, limits=c(0,200000), breaks=c(0, 25000, 50000, 75000, 100000, 125000, 150000, 175000, 200000)) +
+      coord_flip() + theme_bw()
+
+
 ## SCATTER PLOT NOTES
 # scatterplots - zoom in on the main trends in the scatterplots. Sometimes the default x/y limits aren’t ideal 
 # scatterplots - find ways to deal with over plotting (alpha blending, faceting, heatmaps)
 # scatterplots - independent variable (e.g., windspeed) should be on x axis while dependent variable (e.g.,power output) should be on y axis
 # plots - clean them up, make them professional: 172 style. Daniel has noticed both the good and the bad.
-# scatterplots - color by fault type, not just existence of a faul
+# scatterplots - color by fault type, not just existence of a fault
 # PLOTS PLOTS PLOTS, MULTIDIMENSIONAL ONES (scatterplots, but also incorporate color,facets) and then models based on that
 # Do a decent scatterplot matrix, please: https://plotly.com/ggplot2/splom/
 
