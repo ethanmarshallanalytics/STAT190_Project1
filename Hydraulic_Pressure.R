c906d899c4d67aa63afe98321e3d4ec61a510caf
ethanmarshallanalytics <ethan.marshall@drake.edu>
2023-03-07 15:13:20 -0600

    Create Hydraulic_Pressure.R

---
+++ Hydraulic_Pressure.R
@@ -0,0 +1,23 @@
+# Hydraullic Pressure data
+rm(list = ls())
+library(tidyverse)
+library(lubridate)
+library(readr)
+
+hp_files <- list.files("Hydraulic Pressure/Hydraulic Pressure")
+hp_files
+
+combined_data_hp <- data.frame()
+
+# Loop through the list of files
+for (file in hp_files[-1]) {
+  
+  
+  data <- read.csv(paste0("Hydraulic Pressure/Hydraulic Pressure/", file), header = FALSE)
+  
+  # Append the data to the combined_data data frame
+  combined_data_hp <- rbind(combined_data_hp, data)
+}
+
+  # Write the combined data to a file
+write.csv(combined_data_hp, "Project1Data/hydraulic_pressure.csv")
