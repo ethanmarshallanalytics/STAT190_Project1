6ad453e929e0146a95571f9fd5d5901c81b8059a
Nick PIttman <nicholasjohn.pittman@gmail.com>
2023-04-03 10:56:27 -0500

    New Gearbox Data

--- Gearbox Data.R
+++ Gearbox Data.R
@@ -7,7 +7,7 @@ library(readr)
 
 
 ## -----------------------------Gearbox Bearing Part 1
-sensor_files_1 <- list.files("Gearbox HS Bearing Temp Part 1/Drake Analytics Data Sets 2 Part 1")
+sensor_files_1 <- list.files("Project1Data/Wind Turbine Data Batch 2 - Gearbox HS Bearing/Gearbox HS Bearing")
 sensor_files_1
 
 combined_data_1 <- data.frame()
@@ -16,18 +16,18 @@ combined_data_1 <- data.frame()
 for (file in sensor_files_1[-1]) {
   
   
-  data <- read.csv(paste0("Gearbox HS Bearing Temp Part 1/Drake Analytics Data Sets 2 Part 1/", file), header = FALSE)
-  
+  data <- read.csv(paste0("Project1Data/Wind Turbine Data Batch 2 - Gearbox HS Bearing/Gearbox HS Bearing/", file), header = FALSE)
+  print(nrow(data))
   # Append the data to the combined_data data frame
   combined_data_1 <- rbind(combined_data_1, data)
 }
 
 # Write the combined data to a file
-write.csv(combined_data_1, "Gearbox HS Bearing Temp Part 1/gearbox_1.csv")
+write.csv(combined_data_1, "Project1Data/gearbox_1_p2.csv")
 
 
 ## ------------------------------------------Gearbox Bearing Part 2
-sensor_files_2 <- list.files("Gearbox HS Bearing Temp Part 2/Drake Analytics Data Sets 2 Part 2")
+sensor_files_2 <- list.files("Project1Data/Wind Turbine Data Batch 2 - Gearbox IMS Bearing 1/Gearbox IMS Bearing 1")
 sensor_files_2
 
 combined_data_2 <- data.frame()
@@ -36,18 +36,18 @@ combined_data_2 <- data.frame()
 for (file in sensor_files_2[-1]) {
   
   
-  data <- read.csv(paste0("Gearbox HS Bearing Temp Part 2/Drake Analytics Data Sets 2 Part 2/", file), header = FALSE)
-  
+  data <- read.csv(paste0("Project1Data/Wind Turbine Data Batch 2 - Gearbox IMS Bearing 1/Gearbox IMS Bearing 1/", file), header = FALSE)
+  print(nrow(data))
   # Append the data to the combined_data data frame
   combined_data_2 <- rbind(combined_data_2, data)
 }
 
 # Write the combined data to a file
-write.csv(combined_data_2, "Gearbox HS Bearing Temp Part 2/gearbox_2.csv")
+write.csv(combined_data_2, "Project1Data/gearbox_2_p2.csv")
 
 
 ## ------------------------------------------Gearbox Bearing Part 3
-sensor_files_3 <- list.files("Gearbox HS Bearing Temp Part 3/Gearbox HS Bearing Temp Part 3")
+sensor_files_3 <- list.files("Project1Data/Wind Turbine Data Batch 2 - Gearbox IMS Bearing 2/Gearbox IMS Bearing 2")
 sensor_files_3
 
 combined_data_3 <- data.frame()
@@ -55,15 +55,15 @@ combined_data_3 <- data.frame()
 # Loop through the list of files
 for (file in sensor_files_3[-1]) {
   
-  data <- read.csv(paste0("Gearbox HS Bearing Temp Part 3/Gearbox HS Bearing Temp Part 3/", file), header = FALSE)
-  
+  data <- read.csv(paste0("Project1Data/Wind Turbine Data Batch 2 - Gearbox IMS Bearing 2/Gearbox IMS Bearing 2/", file), header = FALSE)
+  print(nrow(data))
   # Append the data to the combined_data data frame
   combined_data_3 <- rbind(combined_data_3, data)
 }
 
 
 # Write the combined data to a file
-write.csv(combined_data_3, "Gearbox HS Bearing Temp Part 3/gearbox_3.csv")
+write.csv(combined_data_3, "Project1Data/gearbox_3_p2.csv")
 
 ## ------------------------------------------Gearbox Oil Temp
 sensor_files_4 <- list.files("Project1Data/Gearbox Oil Temperature")
