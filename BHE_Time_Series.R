1a8f14e760c1b72c9d13c0b928d863633c3c4529
Nick PIttman <nicholasjohn.pittman@gmail.com>
2023-04-12 10:09:22 -0500

    Time series interpolation for missing vales
    written to master_data.csv

--- BHE_Time_Series.R
+++ BHE_Time_Series.R
@@ -10,10 +10,22 @@ library(naniar)
 library(imputeTS)
 
 # Read in Plotting Data
-plot_data = read.csv("Project1Data/plotting_data.csv")
+plot_data = read.csv("Project1Data/plotting_data_1.csv")
+
+# Change data types and remove extra columns
+plot_data$Round_Time <- ymd_hms(plot_data$Round_Time) # change from chr to datetime
+plot_data <- subset(plot_data, select = -c(Wind_Speed_Group, delta_temp)) 
+
 
 ## TIME SERIES MODEL ----
 master_data = plot_data
 
-# na_interpolation(master_data$Wind_Speed)
-# ggplot_na_distribution(file)
+master_data$Oil_Temp = na_interpolation(master_data$Oil_Temp)
+master_data$Generator_RPM = na_interpolation(master_data$Generator_RPM)
+master_data$Wind_Speed = na_interpolation(master_data$Wind_Speed)
+master_data$Gearbox_Temp = na_interpolation(master_data$Gearbox_Temp)
+master_data$Active_Power = na_interpolation(master_data$Active_Power)
+master_data$Ambient_Temp = na_interpolation(master_data$Ambient_Temp)
+master_data$Hydraulic_Pressure = na_interpolation(master_data$Hydraulic_Pressure)
+
+write.csv(master_data, "Project1Data/master_data.csv", row.names=F)
