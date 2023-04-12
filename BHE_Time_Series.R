5a2db0a8e0bd49977f4faf64d18274d9a0c3fccd
ethanmarshallanalytics <ethan.marshall@drake.edu>
2023-04-12 10:22:06 -0500

    Lag on Master Data

--- BHE_Time_Series.R
+++ BHE_Time_Series.R
@@ -29,3 +29,22 @@ master_data$Ambient_Temp = na_interpolation(master_data$Ambient_Temp)
 master_data$Hydraulic_Pressure = na_interpolation(master_data$Hydraulic_Pressure)
 
 write.csv(master_data, "Project1Data/master_data.csv", row.names=F)
+
+## Lag Stuff ------------
+master_data <- master_data %>%
+  group_by(Turbine) %>%
+  arrange(Round_Time) %>%
+  mutate(prev_oil_temp = lag(Oil_Temp, n = 36)) %>%
+  mutate(prev_generator_RPM = lag(Generator_RPM, n = 36)) %>%
+  mutate(prev_wind_speed = lag(Wind_Speed, n = 36)) %>%
+  mutate(prev_gearbox_temp = lag(Gearbox_Temp, n = 36)) %>%
+  mutate(prev_active_power = lag(Active_Power, n = 36)) %>%
+  mutate(prev_ambient_temp = lag(Ambient_Temp, n = 36)) %>%
+  mutate(prev_hydraulic_pressure = lag(Hydraulic_Pressure, n = 36))
+
+# Reorder Columns
+master_data <- master_data[c("Turbine", "Datetime", "Date", "Fault_Code", "Status",
+                             "Fault_Description", "Fault_Type", "Round_Time", "Oil_Temp", "prev_oil_temp",
+                             "Generator_RPM", "prev_generator_RPM", "Wind_Speed", "prev_wind_speed", "Gearbox_Temp",
+                             "prev_gearbox_temp", "Active_Power", "prev_active_power", "Ambient_Temp", "prev_ambient_temp",
+                             "Hydraulic_Pressure", "prev_hydraulic_pressure")]
